825ef035c9294a7fdccacf60f3fdf190aef882cc
Bruni12 <49500292+Bruni12@users.noreply.github.com>
2020-05-22 17:30:50 -0600

    Union Types

---
+++ Typescript/009_union_types.js
@@ -0,0 +1,10 @@
+var players = ["Altuve", "Corerra", "Bregman"];
+var player_numbers = [25, 3, 2];
+console.log(players);
+console.log(player_numbers);
+var names;
+names = ["Brenden Brunnette", "Emily Brunnette"];
+console.log(names);
+names = "Mila Brunnette";
+console.log(names);
+//# sourceMappingURL=009_union_types.js.map
