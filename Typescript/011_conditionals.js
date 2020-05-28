aa539d7a34b59cabc50f65c12a4b1774d8a63648
Bruni12 <49500292+Bruni12@users.noreply.github.com>
2020-05-28 14:42:49 -0600

    Conditionals

---
+++ Typescript/011_conditionals.js
@@ -0,0 +1,11 @@
+var password = 'zxzcvzxcv';
+if (password == 'asdfasdf') {
+    console.log('Yes, asdfasdf is the password');
+}
+else if (password == 'zxzcvzxcv') {
+    console.log('Yes, zxzcvzxcv is the password');
+}
+else {
+    console.log('Sorry, permission denied');
+}
+//# sourceMappingURL=011_conditionals.js.map
---
+++ Typescript/011_conditionals.ts
@@ -0,0 +1,9 @@
+let password : string = 'zxzcvzxcv';
+
+if(password == 'asdfasdf') {
+  console.log('Yes, asdfasdf is the password');
+} else if(password == 'zxzcvzxcv') {
+  console.log('Yes, zxzcvzxcv is the password');
+} else {
+  console.log('Sorry, permission denied');   
+}
